d7b9a145e0c3e2c1d5eecfcba07b1cb62f2743cc
Jesse Jurman <j.r.jurman@gmail.com>
2015-06-06 18:41:15 -0500

    js and css and all that fun stuff

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var app = express();
 
+// hosting css
+app.get('/css/styles.css', function (req, res) {
+  res.sendFile(__dirname + '/css/styles.css');
+});
+
 app.get('/player', function (req, res) {
-  res.sendfile(__dirname + '/player/index.html');
+  res.sendFile(__dirname + '/player/index.html');
+});
+
+app.get('/js/player.js', function (req, res) {
+  res.sendFile(__dirname + '/js/player.js');
+});
+
+app.get('/js/pot.js', function (req, res) {
+  res.sendFile(__dirname + '/js/pot.js');
 });
 
 app.get('/pot', function (req, res) {
-  res.sendfile(__dirname + '/pot/index.html');
+  res.sendFile(__dirname + '/pot/index.html');
 });
 
 var server = app.listen(3000, "0.0.0.0", function () {
