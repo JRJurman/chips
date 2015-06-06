0a6617963e62f81f79b6afec819773af057a02fb
Jesse Jurman <j.r.jurman@gmail.com>
2015-06-06 18:07:51 -0500

    players and pots

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/player', function (req, res) {
+  res.sendfile(__dirname + '/player/index.html');
+});
+
+app.get('/pot', function (req, res) {
+  res.sendfile(__dirname + '/pot/index.html');
 });
 
 var server = app.listen(3000, "0.0.0.0", function () {
