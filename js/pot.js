42a81198a33abe1ab365956a20d93d9b45287375
Wilson Chaney <wilsongchaney@gmail.com>
2015-06-06 19:42:17 -0500

    Stupid socket message fix

--- js/pot.js
+++ js/pot.js
@@ -2,7 +2,7 @@ var potBalance = 0;
 
 var socket = io();
 
-socket.on('pot updated',function(newBalance)
+socket.on('update pot',function(newBalance)
 {
 	potBalance = newBalance["balance"];
 	//Update GUI to reflect new balance.
