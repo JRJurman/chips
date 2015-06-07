cd5c50f5cbecc9a5a641015a37f6c4ded73b7716
Wilson Chaney <wilsongchaney@gmail.com>
2015-06-06 22:58:23 -0500

    Login shit

--- app.js
+++ app.js
@@ -55,7 +55,6 @@ io.on('connection', function(socket){
 	{
 		console.log('updating pot in app.js to $'+newAmount);
 		var potJSON = {'balance' : newAmount};
-		console.log('e')
 		io.emit('update pot', potJSON);
 	}
 
