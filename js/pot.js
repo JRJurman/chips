b2086a89b40c5ae159b4fb4aed4721288aa34f1f
Wilson Chaney <wilsongchaney@gmail.com>
2015-06-13 21:14:21 -0500

    Better logging/alerts

--- js/pot.js
+++ js/pot.js
@@ -6,7 +6,7 @@ socket.emit('load pot', {});
 
 socket.on('update pot',function(newBalance)
 {
-	console.log('updating pot to $'+newBalance);
+	console.log('New pot value: $'+newBalance);
 	potBalance = newBalance['balance'];
 	document.getElementById('pot').innerHTML = 'Pot: $'+potBalance;
 });
